63ba25513e111486497c5e1a61d5c16ecf854c1c
Mauricio Labarca <mauriciolabarca4tw@gmail.com>
2016-09-25 20:05:44 -0300

    Add quick and dirty loading msg

--- javascript/narrow.controller.js
+++ javascript/narrow.controller.js
@@ -8,13 +8,16 @@
     var narrowCtrl = this;
     narrowCtrl.found = [];
     narrowCtrl.searchTerm = '';
+    narrowCtrl.loading = false;
 
     var isBlank = function(){return narrowCtrl.searchTerm.trim() == ''};
 
     narrowCtrl.narrowIt = function(){
+      narrowCtrl.loading = true;
       if (isBlank()) {
         narrowCtrl.notFound = true;
         narrowCtrl.found = [];
+        narrowCtrl.loading = false;
         return
       }
 
@@ -22,6 +25,7 @@
       .getMatchedMenuItems(narrowCtrl.searchTerm)
       .then(function(result){
         narrowCtrl.found = result;
+        narrowCtrl.loading = false;
         narrowCtrl.notFound = narrowCtrl.found.length < 1
       });
     }
